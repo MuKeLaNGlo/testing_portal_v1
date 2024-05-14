9d8ce09594e08f6de243d65bee303658b812dd55
Никита Рассказов <vozakssar@yandex.ru>
2024-05-15 00:29:14 +0500

    Добавил настройки админки

--- project/urls.py
+++ project/urls.py
@@ -3,7 +3,6 @@ import os
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from django.views.generic import RedirectView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
@@ -18,7 +17,6 @@ urlpatterns = [
         name='swagger-ui',
     ),
     path('backend/admin/', admin.site.urls),
-    # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
 
 if settings.DEBUG:
